6280b0d9e877ccb937c433303558f7778c9055e3
Karmsheel Ramsugit <karmsheel@gmail.com>
2026-07-18 11:12:44 +0200

    feat: add User Forge Overlord columns

---
+++ prisma/migrations/20260718120000_user_forge_overlord/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN "forgeOverlordProfileKey" TEXT;
+ALTER TABLE "User" ADD COLUMN "forgeOverlordDisplayName" TEXT;
+ALTER TABLE "User" ADD COLUMN "forgeOverlordHermesHome" TEXT;
+ALTER TABLE "User" ADD COLUMN "forgeOverlordSetAt" DATETIME;
